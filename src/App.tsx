4c3713a8c18065574690d81831e9b724697692cd
Ben Shabowski <benshabowski@gmail.com>
2026-01-29 13:31:21 -0600

    Add additional LinkCard components for Synology NAS, TrueNAS, and GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { SiUbiquiti } from "react-icons/si";
+import { SiUbiquiti, SiSynology, SiTruenas } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
 import LinkCard from "./components/LinkCard.tsx";
 
 function App() {
   return (
     <>
         <LinkCard link={'https://unifi.ui.com'} title={'Unifi UI'} icon={<SiUbiquiti size={60}/>} />
+        <LinkCard link={'http://192.168.1.11:5000'} title={'Synology NAS'} icon={<SiSynology size={60}/>} />
+        <LinkCard link={'http://192.168.1.12'} title={'TrueNAS'} icon={<SiTruenas size={60}/>} />
+        <LinkCard link={'https://github.com/ZGameLogic'} title={'Github'} icon={<FaGithub size={60}/>} />
     </>
   )
 }
