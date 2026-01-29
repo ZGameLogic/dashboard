a6926d5c73cef78ac11ccd5a5aefe2b0c9e8820c
Ben Shabowski <benshabowski@gmail.com>
2026-01-29 13:34:01 -0600

    Wrap LinkCard components in a Stack for improved layout and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { SiUbiquiti, SiSynology, SiTruenas } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
 import LinkCard from "./components/LinkCard.tsx";
+import {Stack} from "@mui/material";
 
 function App() {
   return (
-    <>
+      <Stack direction="row" spacing={2} flexWrap="wrap" justifyContent="center" padding={2}>
         <LinkCard link={'https://unifi.ui.com'} title={'Unifi UI'} icon={<SiUbiquiti size={60}/>} />
         <LinkCard link={'http://192.168.1.11:5000'} title={'Synology NAS'} icon={<SiSynology size={60}/>} />
         <LinkCard link={'http://192.168.1.12'} title={'TrueNAS'} icon={<SiTruenas size={60}/>} />
         <LinkCard link={'https://github.com/ZGameLogic'} title={'Github'} icon={<FaGithub size={60}/>} />
-    </>
+      </Stack>
   )
 }
 
